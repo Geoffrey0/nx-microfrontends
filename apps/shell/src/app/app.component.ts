6d57f027305b00667831102beca30bd5d9590750
Geoffrey Beertens <Geoffrey.Beertens@trendminer.com>
2021-08-31 10:54:33 +0200

    feat: implement shell

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UtilService } from '@nx-microfrontends/shared';
 
 @Component({
   selector: 'nx-microfrontends-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'shell';
+export class AppComponent implements OnInit {
+  title = 'My awesome title';
+
+  constructor(private utilService: UtilService) {
+    this.utilService.setPrefix('SHELLPREFIX');
+
+  }
+
+  ngOnInit() {
+    this.title = this.utilService.prefixText(this.title);
+  }
 }
--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -2,11 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { SharedModule } from '@nx-microfrontends/shared';
+
+const ROUTES: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'mfe1',
+    loadChildren: () => loadRemoteModule({
+      remoteName: 'mfe1',
+      exposedModule: './Module'
+    })
+      .then(m => m.RemoteEntryModule)
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES), SharedModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
